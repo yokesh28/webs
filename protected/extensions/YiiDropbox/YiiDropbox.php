004d66c28157e24efc0e5cba01578a3c9bb8bcf9
lokesh <lokesh@s-creative>
2013-08-29 15:05:21 +0530

    Signed-off-by: lokesh <lokesh@s-creative>

--- protected/extensions/YiiDropbox/YiiDropbox.php
+++ protected/extensions/YiiDropbox/YiiDropbox.php
@@ -6,8 +6,8 @@
  * @author Joe Constant <http://joeconstant.me/>
  */
 //Yii::import('application.extensions.oauth2.src.OAuth2.Autoloader');
-include_once "protected/extensions/oauth-php/library/OAuthStore.php";
-include_once "protected/extensions/oauth-php/library/OAuthRequester.php";
+require_once "protected/extensions/oauth-php/library/OAuthStore.php";
+require_once "protected/extensions/oauth-php/library/OAuthRequester.php";
 
 //require_once 'protected/extensions/oauth2/src/OAuth2/Autoloader.php';
 //OAuth2\Autoloader::register();
